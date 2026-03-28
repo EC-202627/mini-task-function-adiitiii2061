17fcad7196a2c6e58e0688ec774dbd6cdda703c9
adiitiii2061 <aditi.meena@avantika.edu.in>
2026-03-28 14:38:27 +0000

    Completed all 5 fine calculation problems

---
+++ Problem_04.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
+    fine = days_overdue * daily_rate
+
+    if fine > max_fine:
+        fine = max_fine
+
+    return fine
+
+
+book_title = " ".join(sys.argv[1:-2])
+days_overdue = int(sys.argv[-2])
+daily_rate = float(sys.argv[-1])
+
+fine = calculate_fine(book_title, days_overdue, daily_rate)
+
+print("Book:", book_title)
+print("Days overdue:", days_overdue)
+print("Fine: Rs.", float(fine))
+
+
+
+
