01b0152044620ec1a04d00d3edbbbc5b63a26d3a
adiitiii2061 <aditi.meena@avantika.edu.in>
2026-03-28 12:33:05 +0000

    Updated all 5 files

--- DNY_03.py
+++ DNY_03.py
@@ -1,1 +1,18 @@
+def calculate_fine(days_overdue, daily_rate=5.0, max_fine=150.0):
+    fine = days_overdue * daily_rate
+    if fine > max_fine:
+        fine = max_fine
+    return fine
 
+data = input().split()
+
+book_title = "".join(data[:-1])
+days = int(data[-1])
+
+fine = calculate_fine(days)
+
+print("Books:",book_title,"Days overdue:", days,"fine:Rs.", fine)
+
+if fine == 150.0:
+    print ("you have accumulated the maximum fine of INR: 150.0")
+    
--- DNY_04.py
+++ DNY_04.py
@@ -1,1 +1,19 @@
+def calculate_fine(days_overdue, daily_rate=150.0):
+    max_fine = 150.0
+    fine = days_overdue * daily_rate
+    if fine > max_fine:
+        fine = max_fine
+    return fine
+
+data = input().split()
+
+book_title = " ".join(data[:-2])
+days = int(data[-2])
+rate = float(data[-1])
+
+fine = calculate_fine(days, rate)
+
+print("Book:", book_title, "Days overdue:", days, "fine:Rs.", fine)
+
+
 
